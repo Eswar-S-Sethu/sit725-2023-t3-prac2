1d0832ebae44f82afc81447ee1423db21b82dd92
Eswar S Sethu <63389431+Eswar-S-Sethu@users.noreply.github.com>
2023-11-17 10:52:48 +1100

    updates

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ app.listen(port,()=>{
     console.log(`App listening to: ${port}`);
 })
 
+app.use(express.static(__dirname+"/"))
+
+app.get("/", (res,res)=>{
+    res.render("index.html")
+});
+
 app.get("/addTwoNumbers",(req,res)=>{
     res.json({
         status:true,
